1bffc2d634fb9455101231734d0a51ad5ae6644a
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-12-20 21:09:27 +0600

    navbar initiated

--- src/components/LastestNews.jsx
+++ src/components/LastestNews.jsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 
 const LastestNews = () => {
     return (
-        <div className="w-11/12 mx-auto">
+        <div className="w-11/12 mx-auto my-5">
             <div className="flex items-center bg-base-300 p-4 gap-4">
                 <p className="bg-secondary p-3 text-base-100">Latest</p>
                 <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import LastestNews from "../components/LastestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
@@ -10,6 +11,9 @@ const HomeLayout = () => {
                 <section>
                     <LastestNews></LastestNews>
                 </section>
+                <nav className="w-11/12 mx-auto">
+                    <Navbar></Navbar>
+                </nav>
             </header>
             <main>
                 <section className="left_nav"></section>
