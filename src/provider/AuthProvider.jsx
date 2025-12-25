9784a051471dc4f8b46b0441130fd24c144ad238
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-12-25 22:56:14 +0600

    user creation added by firebase  while registering

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import app from "../components/firebase/firebase.init";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState({
-        name: 'tanvir mia',
-        email: 'tanvirmia@gmail.com'
-    });
+    const [user, setUser] = useState();
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     const authData = {
         user,
         setUser,
+        createUser,
     };
 
     return <AuthContext value={authData}>
