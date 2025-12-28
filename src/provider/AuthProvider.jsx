4551253da39d17055d57ac1957a0421e78f8058f
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-12-28 20:52:56 +0600

    login featured added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import app from "../components/firebase/firebase.init";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { AuthContext } from "../context/AuthContext";
 
 const auth = getAuth(app);
@@ -13,10 +13,14 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
     const logOut = () => {
         return signOut(auth);
     }
-    
+
     useEffect(() => {
         const unsubscirbe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
@@ -24,12 +28,13 @@ const AuthProvider = ({ children }) => {
         return () => {
             unsubscirbe()
         }
-    },[])
+    }, [])
 
     const authData = {
         user,
         setUser,
         createUser,
+        signIn,
         logOut,
     };
 
