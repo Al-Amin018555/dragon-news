dfe157d8b71b738f792ac4f7dfbba1af463a1baa
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-02 22:26:26 +0600

    firebase configuration hidden

--- src/components/firebase/firebase.init.js
+++ src/components/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4JO2aJBUc7qS_GH53q63EGF8vYw1wgOk",
-  authDomain: "dragon-news-f03ed.firebaseapp.com",
-  projectId: "dragon-news-f03ed",
-  storageBucket: "dragon-news-f03ed.firebasestorage.app",
-  messagingSenderId: "704804324962",
-  appId: "1:704804324962:web:85532ad52d12817987733b"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
