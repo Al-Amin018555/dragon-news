706fc5182273b3fedf7942bef9ca45121575e8f8
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-02 21:31:46 +0600

    added loading spinner for application perfromance boost

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -7,6 +7,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import NewsDetails from "../pages/NewsDetails";
 import PrivateRoute from "../provider/PrivateRoute";
+import Loading from "../pages/Loading";
 
 const router = createBrowserRouter([
     {
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
                 path: '/category/:id',
                 element: <CategoryNews></CategoryNews>,
                 loader: () => fetch('/news.json'),
+                hydrateFallbackElement: <Loading></Loading>,
 
             }
         ]
@@ -48,6 +50,7 @@ const router = createBrowserRouter([
             const res = await fetch('/news.json');
             return res.json();
         },
+        hydrateFallbackElement: <Loading></Loading>,
     },
     {
         path: '/*',
